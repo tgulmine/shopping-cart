e4e3d85bcce94bff1a3839374f67aa88a4d837ec
tgulmine <tgulmine@gmail.com>
2020-07-05 05:20:46 -0300

    add axios & basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import './styles/main.scss';
+import axios from 'axios';
+import Header from './components/Header/Header';
+import ProductCard from './components/ProductCard/ProductCard';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
 const App: React.FC = () => {
   return (
     <div>
+      <Header />
       <div className="App mt-2 text-2xl font-bold">Hello</div>
       <div className="mt-2 text-gray-nanana">world!</div>
     </div>
---
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './styles/main.scss';
+
+const ShoppingCart: React.FC = () => {
+  return (
+    <div>
+      <div className="mt-2 text-2xl font-bold">Hello</div>
+      <div className="mt-2 text-gray-nanana">world!</div>
+    </div>
+  );
+};
+
+export default ShoppingCart;
